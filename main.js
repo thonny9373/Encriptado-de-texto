8af79446dc9552d816f6b79e6a3196c114d7db0a
Thonny Cardenas <thonnycardenas@gmail.com>
2024-06-29 19:30:42 +0200

    arreglos al style.css

--- main.js
+++ main.js
@@ -1,47 +1,66 @@
-document.getElementById('encriptarBtn').addEventListener('click', function(event) {
-  event.preventDefault();
-  encriptarTexto();
-});
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('encriptarBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    encriptarTexto();
+  });
+  
+  document.getElementById('desencriptarBtn').addEventListener('click', function(event) {
+    event.preventDefault();
+    desencriptarTexto();
+  });
 
-document.getElementById('desencriptarBtn').addEventListener('click', function(event) {
-  event.preventDefault();
-  desencriptarTexto();
-});
-
-document.getElementById('inputText').addEventListener('input', function(event) {
-  const value = event.target.value;
-  if (/[A-Z0-9áéíóúÁÉÍÓÚñÑ!@#$%^&*(),.?":{}|<>;~`+_\-=[\]\\|/¡¿]/.test(value)) {
+  document.getElementById('inputText').addEventListener('input', function(event) {
+    const value = event.target.value;
+    if (/[A-Z0-9áéíóúÁÉÍÓÚñÑ!@#$%^&*(),.?":{}|<>;~`+_\-=[\]\\|/¡¿]/.test(value)) {
       alert('Solo letras minúsculas, sin acentos, puntos ni caracteres especiales');
       event.target.value = value.replace(/[A-Z0-9áéíóúÁÉÍÓÚñÑ!@#$%^&*(),.?":{}|<>;~`+_\-=[\]\\|/¡¿]/g, '');
-  }
-});
+    }
+    ajustarAltura(event.target);
+  });
 
-function encriptarTexto() {
-  let texto = document.getElementById('inputText').value;
-  let textoEncriptado = texto
+  function encriptarTexto() {
+    let texto = document.getElementById('inputText').value;
+    let textoEncriptado = texto
       .toLowerCase()
       .replace(/e/g, 'enter')
       .replace(/i/g, 'imes')
       .replace(/a/g, 'ai')
       .replace(/o/g, 'ober')
       .replace(/u/g, 'ufat');
-  document.getElementById('outputText').value = textoEncriptado;
-}
+    const outputText = document.getElementById('outputText');
+    outputText.value = textoEncriptado;
+    ajustarAltura(outputText);
+  }
 
-function desencriptarTexto() {
-  let texto = document.getElementById('inputText').value;
-  let textoDesencriptado = texto
+  function desencriptarTexto() {
+    let texto = document.getElementById('inputText').value;
+    let textoDesencriptado = texto
       .toLowerCase()
       .replace(/enter/g, 'e')
       .replace(/imes/g, 'i')
       .replace(/ai/g, 'a')
       .replace(/ober/g, 'o')
       .replace(/ufat/g, 'u');
-  document.getElementById('outputText').value = textoDesencriptado;
-}
+    const outputText = document.getElementById('outputText');
+    outputText.value = textoDesencriptado;
+    ajustarAltura(outputText);
+  }
 
-document.getElementById('copiarBtn').addEventListener('click', function() {
-  const outputText = document.getElementById('outputText');
-  outputText.select();
-  document.execCommand('copy');
+  document.getElementById('copiarBtn').addEventListener('click', function() {
+    const outputText = document.getElementById('outputText');
+    outputText.select();
+    document.execCommand('copy');
+  });
+
+  function ajustarAltura(textarea) {
+    textarea.style.height = 'auto';
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  }
+
+  // Ajustar altura inicialmente en caso de que haya algún texto
+  const textareas = document.querySelectorAll('textarea');
+  textareas.forEach(textarea => {
+    ajustarAltura(textarea);
+    textarea.addEventListener('input', e => ajustarAltura(e.target));
+  });
 });
